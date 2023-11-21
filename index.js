0264bcc4e3854f173a5049cf9c01d991c051ea61
tshepoandi <lovekilledtshepo@gmail.com>
2023-11-21 09:11:07 +0200

    product controller

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose")
 const dotenv = require("dotenv").config()
 
 const authController = require("./controllers/authController")
+const productController = require("./controllers/productController")
 const app = express()
 const url = `mongodb+srv://tshepomashiloane869:${process.env.MONGO_DB_PASS}@cluster0.lyeqzd4.mongodb.net/?retryWrites=true&w=majority`
 
@@ -12,8 +13,9 @@ mongoose.connect(url).then(() => {
 })
 
 //routes and middlewares
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use('/auth', authController)
-
+app.use('/product', productController)
 app.listen(3000, () => console.log("server has started"))
