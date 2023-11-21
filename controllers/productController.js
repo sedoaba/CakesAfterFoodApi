139cff3b2f544699fff936bd25a76cfdb407c432
tshepoandi <lovekilledtshepo@gmail.com>
2023-11-21 09:10:50 +0200

    product controller

--- controllers/productController.js
+++ controllers/productController.js
@@ -22,3 +22,14 @@ productController.get('/find/:productid', verifyToken, async(req, res) => {
         console.error(error);
     }
 })
+
+productController.post('/', verifyTokenAdmin, async(req, res) => {
+    try {
+        const newProduct = await Product.create({...req.body })
+        return res.status(201).json(newProduct)
+    } catch (error) {
+        console.error(error);
+    }
+})
+
+module.exports = productController
